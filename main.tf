dfff23d48524ecd8eed450f012eb7cf8d142fa51
Halbert Nakagawa <halbert@Halberts-MacBook-Air.local>
2021-09-30 01:41:28 +0200

    - updating provider versions to support apple silicon m1 chip - adding vpc and subnet to allow for terraform apply to work

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 provider "aws" {
   region = var.region
+  # access_key = var.aws_access_key_id
+  # secret_key = var.aws_secret_access_key
+}
+
+resource "aws_vpc" "vpc" {
+ cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
 }
 
 data "aws_ami" "ubuntu" {
@@ -21,6 +32,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  subnet_id = aws_subnet.main.id
 
   tags = {
     Name = var.instance_name
