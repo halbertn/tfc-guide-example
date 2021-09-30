17b1e1331a0268a361d76502d84fffa28db5e5f8
Halbert Nakagawa <halbert@Halberts-MacBook-Air.local>
2021-09-30 18:36:57 +0200

    - cleanup vpc and subnet resource blocks

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 provider "aws" {
   region = var.region
-  # access_key = var.aws_access_key_id
-  # secret_key = var.aws_secret_access_key
 }
 
 resource "aws_vpc" "vpc" {
- cidr_block = "10.0.0.0/16"
+ cidr_block = "10.0.0.0/24"
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24"
 }
 
 data "aws_ami" "ubuntu" {
